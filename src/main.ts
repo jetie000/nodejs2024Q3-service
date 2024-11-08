0f6e545fb1df542b9c05d3ec4f9626d8849c579b
jetie000 <makeenko.005@gmail.com>
2024-11-08 17:19:51 +0300

    docs: add swagger docs, change yaml file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { parse } from 'yamljs';
+import { join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+  console.log(join(__dirname, '..', 'doc', 'api.yaml'));
+
+  const fileContents = readFileSync(
+    join(__dirname, '..', 'doc', 'api.yaml'),
+    'utf8',
+  );
+  const swaggerDocument = parse(fileContents);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   const PORT = configService.get<string>('PORT') || 4000;
   await app.listen(PORT);
 }
